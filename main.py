34c2cbf5782b4963a5ddf015ea7ad7ac2a471c3b
Raklit <raklitnewmail@gmail.com>
2022-01-18 00:10:25 +0800

    compare with another model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import io
 import pandas as pd
 import numpy as np
+from collections import OrderedDict
 
 from Bio import SeqIO
 import graphviz
@@ -15,12 +16,12 @@ def read_file_into_lists(handle : io.TextIOWrapper) -> tuple:
         ids.append(record.id)
         descs.append(record.description)
         seqs.append(record.seq)
-  return ids, descs, seqs
+  str_seq = list(map(str, seqs))
+  return ids, descs, str_seq
 
-
-def main():
-    pos_file_path = "T6SE_training_data/T6SE_Training_Pos_138.fasta"
-    neg_file_path = "T6SE_training_data/T6SE_Training_Neg_1112.fasta"
+def read_data_for_training() -> pd.DataFrame:
+    pos_file_path = "data/T6SE_training_data/T6SE_Training_Pos_138.fasta"
+    neg_file_path = "data/T6SE_training_data/T6SE_Training_Neg_1112.fasta"
     ids, descs, seqs, isPositive = [], [], [], []
 
     # считывание
@@ -43,19 +44,49 @@ def main():
 
     ids, descs = np.array(ids, dtype=str), np.array(descs)
     isPositive = np.array(isPositive, dtype=bool)
-    seqs = np.array(list(map(str, seqs)), dtype=str)
 
     # DataFrame
 
     names = ["id", "desc", "seq", "is_positive"]
     data = pd.DataFrame(data=np.c_[ids, descs, seqs, isPositive], columns=names)
+    return data
 
-    X_train, X_test, y_train, y_test = train_test_split(seqs, isPositive, test_size=0.33, random_state=0)
+def read_data_for_compare() -> pd.DataFrame:
+    feature_file_path = "data/for_compare/ids_and_features.csv"
+    seq_file_path = "data/for_compare/ids_and_seqs.faa"
+    with open(seq_file_path, mode='r', encoding='utf-8') as handle:
+        ids, descs, seqs = read_file_into_lists(handle)
+    temp = OrderedDict([(i, [seq]) for i, seq in zip(ids,seqs)])
+    del ids, descs, seqs
+    temp_df = pd.read_csv(feature_file_path)
+    n = len(temp_df)
+    for i in range(n):
+        row = temp_df.iloc[i]
+        name = row["Name"][1::]
+        aac, dpc, qso, t6se = row["AAC"], row["DPC"], row["QSO"], str(row["T6SE"] == "Yes")
+        temp[name].extend([aac, dpc, qso, t6se])
+    rows = [[key,*temp[key]] for key in temp.keys()]
+    del temp
+    data = pd.DataFrame(columns=("id", "seq", "AAC", "DPC", "QSO", "is_positive"))
+    for i in range(n):
+        data.loc[i] = rows[i]
+    return data
+
+
+def main():
+    data = read_data_for_training()
+    X, y = data["seq"].to_numpy(), data["is_positive"].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
     model = MultiModel()
     
     model.fit(X_train, y_train)
     print(model.score(X_test, y_test))
+    
+    data = read_data_for_compare()
+    X_comp = data["seq"].to_numpy()
+    y_true = data["is_positive"].to_numpy()
+    print(model.score(X_comp, y_true))
 
 if __name__ == '__main__':
     main()
